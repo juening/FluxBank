5ce44477ec60c3b199f11601c213bfda8c89ac53
Jue Ning <juening@gmail.com>
2016-09-23 21:27:23 -0400

    flux done

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: [
+    './source/App.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+      query:
+        {
+          presets:['react', 'es2015']
+        }
+    }]
+  }
+}
